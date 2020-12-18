21337702f1633d276e1e67bd0ed043ca55933104
RezbanAndreiTakeOff <andrei.rezban@takeofflabs.com>
2020-12-18 15:52:02 +0200

    add a menu in main

--- lucrarea11/figuri.cpp
+++ lucrarea11/figuri.cpp
@@ -154,53 +154,152 @@ Cerc *Cerc::c = NULL;
 
 int main(){
 
-  Punct a(2,0), b(0,2), c(0,-2), d(-2,0), e(1,-1),o(0,0);
-  cout<<"\n\nTriunghi:\n\n";
-  Triunghi t(a,b,c);
-  cout<<"Punctele triunghiului sunt:\n";
-  t.afiseazaPuncte();
-  cout<<"\nPerimetrul triunghiului este: "<<t.perimetru()<<"\n";
-  cout<<"O latura a triunghiului are panta: "<<pantaDreptei(t)<<"\n";
-
-  cout<<"\n\nPatrulater:\n\n";
-  Patrulater p(a,b,c,d);
-  cout<<"Punctele patrulaterului sunt:\n";
-  p.afiseazaPuncte();
-  cout<<"\nPerimetrul patrulaterului este: "<<p.perimetru()<<"\n";
-  cout<<"O latura a patrulaterului are panta: "<<pantaDreptei(p)<<"\n";
-
-  cout<<"\n\nPentagon:\n\n";
-  Pentagon pg(a,b,c,d,e);
-  cout<<"Punctele pentagonului sunt:\n";
-  pg.afiseazaPuncte();
-  cout<<"\nPerimetrul pentagonului este: "<<pg.perimetru()<<"\n";
-  cout<<"O latura a pentagonului are panta: "<<pantaDreptei(pg)<<"\n";
-
-  try{
-    cout<<"\n\nCerc\n\n";
-    Cerc *c = Cerc::creeazaCerc(o,2);
-    cout<<"\nPerimetrul cercului este: "<<c->perimetru()<<"\n";
-    if(figuraPeCerc(*c,t))
-      cout<<"Triunghiul este pe cerc!\n";
-    else
-      cout<<"Triunghiul nu este pe cerc!\n";
-
-    if(figuraPeCerc(*c,p))
-      cout<<"Patrulaterul este pe cerc!\n";
-    else
-      cout<<"Patrulaterul nu este pe cerc!\n";
-
-    if(figuraPeCerc(*c,pg))
-      cout<<"Paralelogramul este pe cerc!\n";
-    else
-      cout<<"Paralelogramul nu este pe cerc!\n";
-
-
-    Cerc::creeazaCerc(o,1);
-  }
-  catch(int n)
+  Figura figuri[20];
+  Cerc *c = NULL;
+  int op=1,n=0;
+  while(op>=1 && op<=9)
   {
-    cout<<"\n\nExceptie: Nu se mai poate crea inca un cerc!\n";
+    cout<<"\n\nAlege o optiune:\n";
+    cout<<"1. Adauga un triunghi\n";
+    cout<<"2. Adauga un patrulater\n";
+    cout<<"3. Adauga un pentagon\n";
+    cout<<"4. Adauga un cerc\n";
+    cout<<"5. Calculeaza panta laturii unei figuri alese\n";
+    cout<<"6. Vezi perimetrul unei figuri alese\n";
+    cout<<"7. Vezi perimetrul cercului\n";
+    cout<<"8. Schimba unul sau mai multe puncte ale unei figuri alese\n";
+    cout<<"9. Vezi daca o figura aleasa se afla sau nu pe cerc\n";
+    cout<<"Alta valoare -> Exit\n";
+    cin>>op;
+    switch(op)
+    {
+      case 1:
+      {
+        cout<<"Introdu coordonatele celor 3 puncte ale triunghiului:\n";
+        int x,y;
+        Punct *p[3];
+        for(int i=0;i<3;i++)
+        {
+          cout<<"x"<<i+1<<"=";
+          cin>>x;
+          cout<<"y"<<i+1<<"=";
+          cin>>y;
+          p[i] = new Punct(x,y);
+        }
+        Triunghi t(*p[0],*p[1],*p[2]);
+        figuri[n] = t;
+        n++;
+        break;
+      }
+      case 2:
+      {
+        cout<<"Introdu coordonatele celor 4 puncte ale patrulaterului:\n";
+        int x,y;
+        Punct *p[4];
+        for(int i=0;i<4;i++)
+        {
+          cout<<"x"<<i+1<<"=";
+          cin>>x;
+          cout<<"y"<<i+1<<"=";
+          cin>>y;
+          p[i] = new Punct(x,y);
+        }
+        Patrulater pa(*p[0],*p[1],*p[2],*p[3]);
+        figuri[n] = pa;
+        n++;
+        break;
+      }
+      case 3:
+      {
+        cout<<"Introdu coordonatele celor 5 puncte ale pentagonului:\n";
+        int x,y;
+        Punct *p[5];
+        for(int i=0;i<5;i++)
+        {
+          cout<<"x"<<i+1<<"=";
+          cin>>x;
+          cout<<"y"<<i+1<<"=";
+          cin>>y;
+          p[i] = new Punct(x,y);
+        }
+        Pentagon pg(*p[0],*p[1],*p[2],*p[3],*p[4]);
+        figuri[n] = pg;
+        n++;
+        break;
+      }
+      case 4:
+      {
+        try{
+          cout<<"Introdu coordonatele centrului cercului:\n";
+          int x,y,r;
+          cout<<"x"<<"=";
+          cin>>x;
+          cout<<"y"<<"=";
+          cin>>y;
+          Punct o(x,y);
+          cout<<"Introdu lungimea razei cercului:";
+          cin>>r;
+          c = Cerc::creeazaCerc(o,r);
+        }
+        catch(int n)
+        {
+          cout<<"\n\nExceptie: Nu se mai poate crea inca un cerc!\n";
+        }
+        break;
+      }
+      case 5:
+      {
+        cout<<"Introdu indicele figurii pentru care se va calcula panta:\n";
+        int i;
+        cin>>i;
+        cout<<"\nPanta unei laturi din figura "<<i<<" este "<<pantaDreptei(figuri[i])<<"\n";
+        break;
+      }
+      case 6:
+      {
+        cout<<"Introdu indicele figurii pentru care se va calcula perimetrul (intre 0 si "<<n<<"):\n";
+        int i;
+        cin>>i;
+        cout<<"\nPerimetrul figurii "<<i<<" este "<<figuri[i].perimetru()<<"\n";
+        break;
+      }
+      case 7:
+      {
+        cout<<"\nPerimetrul cercului este "<<c->perimetru()<<"\n";
+        break;
+      }
+      case 8:
+      {
+        cout<<"Introdu indicele figurii pentru care se vor schimba punctele (intre 0 si "<<n<<"):\n";
+        int i,x,y;
+        cin>>i;
+        list <Punct> puncte, puncte_noi;
+        puncte = figuri[i].puncteFigura();
+        list<Punct>::iterator p;
+        for(p = puncte.begin(); p != puncte.end(); ++p)
+        {
+          cout<<"Reintroduceti valorile punctului ("<<p->getX()<<","<<p->getY()<<"):";
+          cin>>x>>y;
+          Punct aux(x,y);
+          puncte_noi.push_front(aux);
+        }
+        figuri[i].seteazaPuncte(puncte_noi);
+        cout<<"\nPunctele figurii "<<i<<" au fost modificate.\n";
+        break;
+      }
+      case 9:
+      {
+        cout<<"Introdu indicele figurii pentru care se va verifica daca se afla pe cerc (intre 0 si "<<n<<"):\n";
+        int i;
+        cin>>i;
+        cout<<"Figura "<<i;
+        if(!figuraPeCerc(*c,figuri[i]))
+          cout<<" nu";
+        cout<<" se afla pe cerc\n";
+        break;
+      }
+      default: break;
+    }
   }
   return 0;
 }
